14cb5bfbf22f58aadb817d7c48991e16fd1937e1
Scott Warner <Tortel1210@gmail.com>
2017-09-06 10:14:25 -0400

    x2: Switch to Lineage vendor
    Change-Id: Idecb9a3565efdaa100a082bfbe01c499c382f760

--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,8 +20,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/leeco/x2/device.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_x2
 PRODUCT_DEVICE := x2
