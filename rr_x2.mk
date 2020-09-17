d6b63add0934124256f80dc39161efd60eb8c1ca
CrisBalGreece <crisbaladim@yahoo.gr>
2020-09-17 09:56:24 +0300

    x2: RR bringup

--- rr_x2.mk
+++ rr_x2.mk
@@ -21,8 +21,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/leeco/x2/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common Resurrection stuff.
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+RR_BUILDTYPE := Official
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := LeEco
