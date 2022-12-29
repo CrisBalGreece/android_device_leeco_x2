027d1b4a9f4e4754b6398ec13ec7c79d72f5c471
CrisBalGreece <crisbaladim@yahoo.gr>
2022-12-29 13:59:08 +0200

    x2:Update fingerprint from raven-13 TP1A.221005.002

--- octavi_x2.mk
+++ octavi_x2.mk
@@ -42,6 +42,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-leeco
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=le_x2 \
     PRODUCT_NAME=LeMax2_WW \
-    #PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003 8671607 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 13 TP1A.221005.002 9012097 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:12/SQ3A.220705.003/8671607:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:13/TP1A.221005.002/9012097:user/release-keys
